20ec66bf5e671627a083a3cfc71d23d9f29c209e
sooin <lilesooin@gmail.com>
2025-11-16 19:39:10 -0500

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller // 컴포넌트 스캔
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -2,11 +2,13 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 // 서비스는 메서드명이 비즈니스와 가까운 메서드명 사용
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
